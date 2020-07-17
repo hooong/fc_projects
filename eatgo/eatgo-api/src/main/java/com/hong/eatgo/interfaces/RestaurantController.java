d0d069d15901ce9f6192340837d8abcc846dd570
hooong <tjrwns0529@gmail.com>
2020-07-17 17:23:37 +0900

    Add Repository

---
+++ eatgo/eatgo-api/src/main/java/com/hong/eatgo/interfaces/RestaurantController.java
@@ -0,0 +1,29 @@
+package com.hong.eatgo.interfaces;
+
+import com.hong.eatgo.domain.Restaurant;
+import com.hong.eatgo.domain.RestaurantRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class RestaurantController {
+
+    private RestaurantRepository repository = new RestaurantRepository();
+
+    @GetMapping("/restaurants")
+    public List<Restaurant> list() {
+
+        List<Restaurant> restaurants = repository.findAll();
+
+        return restaurants;
+    }
+
+    @GetMapping("/restaurant/{id}")
+    public Restaurant detail(@PathVariable("id") Long id){
+        return repository.findById(id);
+    }
+}

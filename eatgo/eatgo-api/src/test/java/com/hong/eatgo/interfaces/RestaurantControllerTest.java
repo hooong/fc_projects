545cf799d082c0ab1209b362a9f0bee659b9da0e
hooong <tjrwns0529@gmail.com>
2020-07-17 17:59:22 +0900

    DI

--- eatgo/eatgo-api/src/test/java/com/hong/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo/eatgo-api/src/test/java/com/hong/eatgo/interfaces/RestaurantControllerTest.java
@@ -1,9 +1,12 @@
 package com.hong.eatgo.interfaces;
 
+import com.hong.eatgo.domain.RestaurantRepository;
+import com.hong.eatgo.domain.RestaurantRepositoryImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,6 +21,9 @@ public class RestaurantControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @SpyBean(RestaurantRepositoryImpl.class)
+    private RestaurantRepository restaurantRepository;
 
     @Test
     public void list() throws Exception {

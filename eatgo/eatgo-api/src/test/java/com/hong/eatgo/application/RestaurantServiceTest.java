e5ae923341df9d27e282fc83df9388333b5df579
hooong <tjrwns0529@gmail.com>
2020-07-19 20:03:11 +0900

    Add: jpa

--- eatgo/eatgo-api/src/test/java/com/hong/eatgo/application/RestaurantServiceTest.java
+++ eatgo/eatgo-api/src/test/java/com/hong/eatgo/application/RestaurantServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
@@ -46,7 +47,7 @@ public class RestaurantServiceTest {
         Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
         restaurants.add(restaurant);
         given(restaurantRepository.findAll()).willReturn(restaurants);
-        given(restaurantRepository.findById(1004L)).willReturn(restaurant);
+        given(restaurantRepository.findById(1004L)).willReturn(Optional.of(restaurant));
     }
 
     private void mockMenuItemRepository() {

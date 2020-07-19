5ca822c82f354c6ab185543f38957c68d073a10e
hooong <tjrwns0529@gmail.com>
2020-07-20 01:05:30 +0900

    Add: MenuItem

---
+++ eatgo/eatgo-api/src/main/java/com/hong/eatgo/interfaces/MenuItemController.java
@@ -0,0 +1,31 @@
+package com.hong.eatgo.interfaces;
+
+import com.hong.eatgo.application.MenuItemService;
+import com.hong.eatgo.domain.MenuItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MenuItemController {
+
+    @Autowired
+    private MenuItemService menuItemService;
+
+    @PatchMapping("/restaurants/{restaurantId}/menuitems")
+    public String bulkUpdate(
+            @PathVariable("restaurantId") Long id,
+            @RequestBody List<MenuItem> menuItems
+    ) {
+        menuItemService.bulkUpdate(id, menuItems);
+
+        return "";
+    }
+
+}

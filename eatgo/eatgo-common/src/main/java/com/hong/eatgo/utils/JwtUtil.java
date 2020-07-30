41272fd40ceb90e5eb4becee09277739e0bacb2f
홍석준 <tjrwns0529@gmail.com>
2020-07-30 16:59:22 +0900

    Separate Login API

--- eatgo/eatgo-common/src/main/java/com/hong/eatgo/utils/JwtUtil.java
+++ eatgo/eatgo-common/src/main/java/com/hong/eatgo/utils/JwtUtil.java
@@ -1,13 +1,12 @@
 package com.hong.eatgo.utils;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.stereotype.Component;
 
 import java.security.Key;
-import java.security.Security;
 
 public class JwtUtil {
 
@@ -17,11 +16,17 @@ public class JwtUtil {
         this.key = Keys.hmacShaKeyFor(secret.getBytes());
     }
 
-    public String createToken(long userId, String name) {
+    public String createToken(long userId, String name, Long restaurantId) {
 
-        return Jwts.builder()
+        JwtBuilder builder = Jwts.builder()
                 .claim("userId", userId)
-                .claim("name", name)
+                .claim("name", name);
+
+        if(restaurantId != null) {
+            builder = builder.claim("restaurantId", restaurantId);
+        }
+
+        return builder
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }

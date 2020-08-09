aebd46ce6a09b8949ab9a1070e925ded5f344314
홍석준 <tjrwns0529@gmail.com>
2020-08-09 20:09:16 +0900

    Create mycontact Proj

---
+++ mycontact/src/test/java/com/hong/mycontact/MycontactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hong.mycontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycontactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

ab0ef9a03e8b39eba0ef058190085d3ad852ad03
홍석준 <tjrwns0529@gmail.com>
2020-08-10 01:35:56 +0900

    domain test

---
+++ mycontact/src/main/java/com/hong/mycontact/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.hong.mycontact.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/api/helloWorld")
+    public String helloWorld() {
+        return "Hello world";
+    }
+}
---
+++ mycontact/src/test/java/com/hong/mycontact/repository/PersonRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.hong.mycontact.repository;
+
+import com.hong.mycontact.domain.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PersonRepositoryTest {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    void crud() {
+        Person person = new Person();
+        person.setName("hong");
+        person.setAge(25);
+
+        personRepository.save(person);
+
+//        System.out.println(personRepository.findAll());
+
+        List<Person> people = personRepository.findAll();
+
+        assertThat(people.size()).isEqualTo(1);
+        assertThat(people.get(0).getName()).isEqualTo("hong");
+        assertThat(people.get(0).getAge()).isEqualTo(25);
+
+    }
+}
